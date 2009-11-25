26d8015b42015251e70a0b006deaf5fc8734e993
Anand Patil <anand.prabhakar.patil@gmail.com>
2009-11-25 16:51:06 +0000

    Runs with multiple fields

--- duffy/__init__.py
+++ duffy/__init__.py
@@ -1,6 +1,6 @@
 # from mcmc import *
 from model import *
-from generic_mbg import invlogit, FieldStepper
+from generic_mbg import invlogit, FieldStepper, fast_inplace_mul, fast_inplace_square
 from cut_geographic import cut_geographic, hemisphere
 import duffy
 import pymc as pm
@@ -8,19 +8,33 @@ import os
 root = os.path.split(duffy.__file__)[0]
 pm.gp.cov_funs.cov_utils.mod_search_path.append(root)
 
- 
 # Stuff mandated by the new map_utils standard
- 
 cut_matern = pm.gp.cov_utils.covariance_wrapper('matern', 'pymc.gp.cov_funs.isotropic_cov_funs', {'diff_degree': 'The degree of differentiability of realizations.'}, 'cut_geographic', 'cg')
  
-diag_safe = True
-f_name = 'eps_p_f'
-x_name = 'data_mesh'
-f_has_nugget = True
-nugget_name = 'V'
+diags_safe = {'eps_p_fb': True, 'eps_p_f0': True}
+f_labels = ['eps_p_fb', 'eps_p_f0']
+x_label = 'data_mesh'
+fs_have_nugget = {'eps_p_fb': True, 'eps_p_f0': True}
+nugget_labels = {'eps_p_fb': 'V_b', 'eps_p_f0': 'V_0'}
+M_labels = {'eps_p_fb': 'M_b', 'eps_p_f0': 'M_0'}
+C_labels = {'eps_p_fb': 'C_b', 'eps_p_f0': 'M_0'}
+covariate_pertenencies = {'eps_p_fb': [], 'eps_p_f0': []}
+
+def postproc(eps_p_fb, eps_p_f0):
+    """
+    Returns probability of Duffy negativity from two random fields giving mutation frequencies.
+    Fast and threaded.
+    """
+    pb = invlogit(eps_p_fb)
+    p0 = invlogit(eps_p_f0)
+    fast_inplace_mul(pb,p0)
+    fast_inplace_square(pb)
+    return pb
+
 metadata_keys = ['fi','ti','ui']
-postproc = invlogit
-step_method_orders = {'f':(FieldStepper, )}
+
+step_method_orders = {'fb':(FieldStepper, 'V_b', 'C_eval_b', 'M_eval_b', 'logp_mesh', 'eps_p_fb', 'ti'),
+                        'f0':(FieldStepper, 'V_0', 'C_eval_0', 'M_eval_0', 'logp_mesh', 'eps_p_f0', 'ti')}
 non_cov_columns = {'africa': 'int',
                     'n': 'int',
                     'datatype': 'str',
--- duffy/model.py
+++ duffy/model.py
@@ -29,7 +29,7 @@ import generic_mbg
 
 __all__ = ['make_model']
 
-def ibd_covariance_submodel():
+def ibd_covariance_submodel(suffix):
     """
     A small function that creates the mean and covariance object
     of the random field.
@@ -38,27 +38,28 @@ def ibd_covariance_submodel():
     # from duffy import cut_matern
     
     # The partial sill.
-    amp = pm.Exponential('amp', .1, value=1.)
+    amp = pm.Exponential('amp_%s'%suffix, .1, value=1.)
     
     # The range parameter. Units are RADIANS. 
     # 1 radian = the radius of the earth, about 6378.1 km
     # scale = pm.Exponential('scale', 1./.08, value=.08)
     
-    scale_shift = pm.Exponential('scale_shift', .1, value=.08)
-    scale = pm.Lambda('scale',lambda s=scale_shift: s+.01)
+    scale_shift = pm.Exponential('scale_shift_%s'%suffix, .1, value=.08)
+    scale = pm.Lambda('scale_%s'%suffix,lambda s=scale_shift: s+.01)
     scale_in_km = scale*6378.1
     
     # This parameter controls the degree of differentiability of the field.
-    diff_degree = pm.Uniform('diff_degree', .01, 1.5)
+    diff_degree = pm.Uniform('diff_degree_%s'%suffix, .01, 1.5)
     
     # The nugget variance.
-    V = pm.Exponential('V', .1, value=1.)
+    V = pm.Exponential('V_%s'%suffix, .1, value=1.)
     
     # Create the covariance & its evaluation at the data locations.
     @pm.deterministic(trace=True)
     def C(amp=amp, scale=scale, diff_degree=diff_degree):
         """A covariance function created from the current parameter values."""
         return pm.gp.FullRankCovariance(pm.gp.matern.geo_rad, amp=amp, scale=scale, diff_degree=diff_degree)
+    C.__name__ = 'C_%s'%suffix
     
     return locals()
     
@@ -97,40 +98,62 @@ def make_model(lon,lat,covariate_values,n,africa,datatype,genaa,genab,genbb,gen0
     logp_mesh = combine_spatial_inputs(lon,lat)
     
     # Create the mean & its evaluation at the data locations.
-    M, M_eval = trivial_means(logp_mesh)
     print data_mesh.shape
     init_OK = False
     
     while not init_OK:
         try:        
+            
+            M_b, M_eval_b = trivial_means(logp_mesh)
+            M_b.__name__ = 'M_b'
+            M_eval_b.__name__ = 'M_eval_b'
+            M_0, M_eval_0 = trivial_means(logp_mesh)
+            M_0.__name__ = 'M_0'
+            M_eval_0.__name__ = 'M_eval_0'
+            
             # Space-time component
-            sp_sub = ibd_covariance_submodel()    
-            covariate_dict, C_eval = cd_and_C_eval(covariate_values, sp_sub['C'], data_mesh, ui)
+            sp_sub_b = ibd_covariance_submodel('b')    
+            sp_sub_0 = ibd_covariance_submodel('0')
+            
+            covariate_dict_b, C_eval_b = cd_and_C_eval(covariate_values, sp_sub_b['C'], data_mesh, ui)
+            C_eval_b.__name__ = 'C_eval_b'
+            covariate_dict_0, C_eval_0 = cd_and_C_eval(covariate_values, sp_sub_0['C'], data_mesh, ui)
+            C_eval_0.__name__ = 'C_eval_0'
         
             # The field evaluated at the uniquified data locations            
-            f = pm.MvNormalCov('f', M_eval, C_eval)
+            fb = pm.MvNormalCov('fb', M_eval_b, C_eval_b)
+            f0 = pm.MvNormalCov('f0', M_eval_0, C_eval_0)
+
             # Make f start somewhere a bit sane
-            f.value = f.value - np.mean(f.value)
+            fb.value = fb.value - np.mean(fb.value)
+            f0.value = f0.value - np.mean(f0.value)            
             
             # Loop over data clusters
-            eps_p_f_d = []
+            eps_p_f0_d = []
             p0_d = []
+            eps_p_fb_d = []
+            pb_d = []
         
             for i in xrange(len(n)):
-            
-                this_f = pm.Lambda('f_%i'%i, lambda f=f, i=i, fi=fi: f[fi[i]], trace=False)
-            
+                this_fb = pm.Lambda('fb_%i'%i, lambda f=fb, i=i, fi=fi: f[fi[i]], trace=False)
+                this_f0 = pm.Lambda('f0_%i'%i, lambda f=f0, i=i, fi=fi: f[fi[i]], trace=False)
                 # Nuggeted field in this cluster
-                eps_p_f_d.append(pm.Normal('eps_p_f_%i'%i, this_f, 1./sp_sub['V'], value=0.,trace=False))
-        
+                eps_p_fb_d.append(pm.Normal('eps_p_fb_%i'%i, this_fb, 1./sp_sub_b['V'], value=0.,trace=False))
+                eps_p_f0_d.append(pm.Normal('eps_p_f0_%i'%i, this_f0, 1./sp_sub_0['V'], value=0.,trace=False))
                 # The allele frequency
-                p0_d.append(pm.Lambda('s_%i'%i,lambda lt=eps_p_f_d[-1]: np.asscalar(invlogit(lt)),trace=False))
+                pb_d.append(pm.Lambda('s_%i'%i,lambda lt=eps_p_fb_d[-1]: np.asscalar(invlogit(lt)),trace=False))
+                p0_d.append(pm.Lambda('s_%i'%i,lambda lt=eps_p_f0_d[-1]: np.asscalar(invlogit(lt)),trace=False))
         
             # The field plus the nugget
             @pm.deterministic
-            def eps_p_f(eps_p_fd = eps_p_f_d):
-                """Concatenated version of eps_p_f, for postprocessing & Gibbs sampling purposes"""
-                return np.hstack(eps_p_fd)
+            def eps_p_fb(eps_p_fb_d = eps_p_fb_d):
+                """Concatenated version of eps_p_fb, for postprocessing & Gibbs sampling purposes"""
+                return np.hstack(eps_p_fb_d)
+
+            @pm.deterministic
+            def eps_p_f0(eps_p_f0_d = eps_p_f0_d):
+                """Concatenated version of eps_p_f0, for postprocessing & Gibbs sampling purposes"""
+                return np.hstack(eps_p_f0_d)
         
             init_OK = True
         except pm.ZeroProbability, msg:
@@ -141,20 +164,26 @@ def make_model(lon,lat,covariate_values,n,africa,datatype,genaa,genab,genbb,gen0
     # The observed allele frequencies
     data_d = []    
     for i in xrange(len(n)):
-        p0=p0_d[i]
-        pb = .5
+
+        # See duffy/doc/model.tex for explanations of the likelihoods.
+        p0 = p0_d[i]
+        pb = pb_d[i]
+        
         if datatype[i]=='gf':
             pass
+            
         elif datatype[i]=='pos':
             cur_obs = [pos0[i], negab[i]]
             p = pm.Lambda('p_%i'%i, lambda pb=pb, p0=p0: (pb*p0)**2, trace=False)
             n = np.sum(cur_obs)
             data_d.append(pm.Binomial('data_%i'%i, p=p, n=n, value=pos0[i], observed=True))
+            
         elif datatype[i]=='aphe':
             cur_obs = [aphea[i], aphe0[i]]
             n = np.sum(cur_obs)
             p = pm.Lambda('p_%i'%i, lambda pb=pb, p0=p0: (1-pb)**2+2*(1-pb)*pb, trace=False)
             data_d.append(pm.Binomial('data_%i'%i, p=p, n=n, value=aphea[i], observed=True))
+            
         elif datatype[i]=='phe':
             cur_obs = np.array([pheab[i],phea[i],pheb[i],phe0[i]])
             n = np.sum(cur_obs)
@@ -164,6 +193,7 @@ def make_model(lon,lat,covariate_values,n,africa,datatype,genaa,genab,genbb,gen0
                 2*pb**2*(1-p0)*p0+(pb*(1-p0))**2,
                 (pb*p0)**2]), trace=False)
             data_d.append(pm.Multinomial('data_%i'%i, p=p, n=n, value=cur_obs, observed=True))
+            
         elif datatype[i]=='gen':
             cur_obs = np.array([genaa[i],genab[i],gen00[i],gena0[i],genb0[i],genbb[i]])
             n = np.sum(cur_obs)
@@ -175,12 +205,19 @@ def make_model(lon,lat,covariate_values,n,africa,datatype,genaa,genab,genbb,gen0
                 2*pb**2*(1-p0)*p0,
                 (pb*(1-p0))**2]), trace=False)
             data_d.append(pm.Multinomial('data_%i'%i, p=p, n=n, value=cur_obs, observed=True))
-
+            
         if np.any(np.isnan(cur_obs)):
             raise ValueError
+        if datatype[i] in ['phe','gen']:
+            np.testing.assert_almost_equal(p.value.sum(), 1)
     
+    covariate_dict = covariate_dict_0
+            
     out = locals()
-    out.pop('sp_sub')
-    out.update(sp_sub)
+    # out.pop('sp_sub_b')
+    # out.pop('sp_sub_0')
+    for d in sp_sub_b.values() + sp_sub_0.values():
+        if isinstance(d,pm.Variable):
+            out[d.__name__] = d
 
     return out
