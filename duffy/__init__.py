60c9e34807ef6aa9b1434d526ba8ddb61b827230
Anand Patil <anand.prabhakar.patil@gmail.com>
2009-11-26 14:09:38 +0000

    Works up through postproc

--- duffy/__init__.py
+++ duffy/__init__.py
@@ -17,24 +17,33 @@ x_label = 'data_mesh'
 fs_have_nugget = {'eps_p_fb': True, 'eps_p_f0': True}
 nugget_labels = {'eps_p_fb': 'V_b', 'eps_p_f0': 'V_0'}
 M_labels = {'eps_p_fb': 'M_b', 'eps_p_f0': 'M_0'}
-C_labels = {'eps_p_fb': 'C_b', 'eps_p_f0': 'M_0'}
+C_labels = {'eps_p_fb': 'C_b', 'eps_p_f0': 'C_0'}
 covariate_pertenencies = {'eps_p_fb': [], 'eps_p_f0': []}
 
-def postproc(eps_p_fb, eps_p_f0):
+def map_postproc(eps_p_fb, eps_p_f0):
     """
     Returns probability of Duffy negativity from two random fields giving mutation frequencies.
     Fast and threaded.
     """
     pb = invlogit(eps_p_fb)
     p0 = invlogit(eps_p_f0)
-    fast_inplace_mul(pb,p0)
-    fast_inplace_square(pb)
+    pb = fast_inplace_mul(pb,p0)
+    print 'OK!'
+    pb = fast_inplace_square(pb)
     return pb
 
+def validate_postproc(**non_cov_columns):
+    """
+    Don't know what to do here yet.
+    """
+    raise NotImplementedError
+    
 metadata_keys = ['fi','ti','ui']
 
-step_method_orders = {'fb':(FieldStepper, 'V_b', 'C_eval_b', 'M_eval_b', 'logp_mesh', 'eps_p_fb', 'ti'),
-                        'f0':(FieldStepper, 'V_0', 'C_eval_0', 'M_eval_0', 'logp_mesh', 'eps_p_f0', 'ti')}
+def mcmc_init(M):
+    M.use_step_method(FieldStepper, M.fb, M.V_b, M.C_eval_b, M.M_eval_b, M.logp_mesh, M.eps_p_fb, M.ti)
+    M.use_step_method(FieldStepper, M.f0, M.V_0, M.C_eval_0, M.M_eval_0, M.logp_mesh, M.eps_p_f0, M.ti)
+
 non_cov_columns = {'africa': 'int',
                     'n': 'int',
                     'datatype': 'str',
