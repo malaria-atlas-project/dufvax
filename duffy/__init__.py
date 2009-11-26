4012f71d78a93b0a33440031013856745465a6a7
Anand Patil <anand.prabhakar.patil@gmail.com>
2009-11-26 16:48:11 +0000

    Going to start writing proper methods section

--- duffy/__init__.py
+++ duffy/__init__.py
@@ -11,25 +11,29 @@ pm.gp.cov_funs.cov_utils.mod_search_path.append(root)
 # Stuff mandated by the new map_utils standard
 cut_matern = pm.gp.cov_utils.covariance_wrapper('matern', 'pymc.gp.cov_funs.isotropic_cov_funs', {'diff_degree': 'The degree of differentiability of realizations.'}, 'cut_geographic', 'cg')
  
-diags_safe = {'eps_p_fb': True, 'eps_p_f0': True}
 f_labels = ['eps_p_fb', 'eps_p_f0']
-x_label = 'data_mesh'
 fs_have_nugget = {'eps_p_fb': True, 'eps_p_f0': True}
 nugget_labels = {'eps_p_fb': 'V_b', 'eps_p_f0': 'V_0'}
 M_labels = {'eps_p_fb': 'M_b', 'eps_p_f0': 'M_0'}
 C_labels = {'eps_p_fb': 'C_b', 'eps_p_f0': 'C_0'}
-covariate_pertenencies = {'eps_p_fb': [], 'eps_p_f0': []}
+x_label = 'data_mesh'
+covariate_pertenencies = {'eps_p_fb': ['africa'], 'eps_p_f0': []}
+diags_safe = {'eps_p_fb': True, 'eps_p_f0': True}
 
-def map_postproc(eps_p_fb, eps_p_f0):
+def map_postproc(eps_p_fb, eps_p_f0, p1):
     """
     Returns probability of Duffy negativity from two random fields giving mutation frequencies.
     Fast and threaded.
+    TODO: Fortran-sticate this.
     """
+    # Probability of a/b switch
     pb = invlogit(eps_p_fb)
+    
+    # Negativity contributions from b individuals
     p0 = invlogit(eps_p_f0)
-    pb = fast_inplace_mul(pb,p0)
-    pb = fast_inplace_square(pb)
-    return pb
+    
+    # All individuals Duffy negative at both chromosomes
+    return (pb*p0 + (1-pb)*p1)**2
 
 def validate_postproc(**non_cov_columns):
     """
@@ -43,8 +47,7 @@ def mcmc_init(M):
     M.use_step_method(FieldStepper, M.fb, M.V_b, M.C_eval_b, M.M_eval_b, M.logp_mesh, M.eps_p_fb, M.ti)
     M.use_step_method(FieldStepper, M.f0, M.V_0, M.C_eval_0, M.M_eval_0, M.logp_mesh, M.eps_p_f0, M.ti)
 
-non_cov_columns = {'africa': 'int',
-                    'n': 'int',
+non_cov_columns = { 'n': 'int',
                     'datatype': 'str',
                     'genaa': 'float',
                     'genab': 'float',
