379b89430e303f91116b9a201ea46560f6d537f1
Anand Patil <anand.prabhakar.patil@gmail.com>
2010-02-01 15:29:13 +0000

    Cloaned up model.py, have not tested

--- dufvax/model.py
+++ dufvax/model.py
@@ -105,6 +105,9 @@ for i in xrange(1000):
     pb,p0,p1 = np.random.random(size=3)
     np.testing.assert_almost_equal(np.sum([gfi(pb,p0,p1) for gfi in g_freqs.values()]),1.)
 
+def zipmap(f, keys):
+    return dict(zip([keys, map(f, keys)]))
+
 #TODO: Cut both Duffy and Vivax    
 def make_model(lon,lat,covariate_values,n,datatype,
                 genaa,genab,genbb,gen00,gena0,genb0,gena1,genb1,gen01,gen11,
@@ -121,23 +124,23 @@ def make_model(lon,lat,covariate_values,n,datatype,
     grainsize = 5
     
     where_duffy = np.where(np.isnan(vivax_pos))
-    duflon, duflat = lon[where_duffy], lat[where_duffy]
-
+    where_vivax = np.where(True-np.isnan(vivax_pos))
+    
     # Rebind input variables for convenience
     # for dlab in ['genaa','genab','genbb','gen00','gena0','genb0','gena1','genb1','gen01','gen11','pheab','phea','pheb','phe0','prom0','promab','aphea','aphe0','bpheb','bphe0']:
     #     exec('%s=%s[where_duffy]'(%dlab,dlab))
         
     # Non-unique data locations
-    duffy_data_mesh = combine_spatial_inputs(duflon, duflat)
+    data_mesh = combine_spatial_inputs(lon, lat)
     
     # Uniquify the data locations.
-    locs = [(duflon[0], duflat[0])]
+    locs = [(lon[0], lat[0])]
     fi = [0]
     ui = [0]
-    for i in xrange(1,len(duflon)):
+    for i in xrange(1,len(lon)):
         
         # If repeat location, add observation
-        loc = (duflon[i], duflat[i])
+        loc = (lon[i], lat[i])
         if loc in locs:
             fi.append(locs.index(loc))
             
@@ -150,11 +153,8 @@ def make_model(lon,lat,covariate_values,n,datatype,
     ti = [np.where(fi == i)[0] for i in xrange(max(fi)+1)]
     ui = np.asarray(ui)
     
-    duflon = np.array(locs)[:,0]
-    duflat = np.array(locs)[:,1]
-    
     # Unique data locations
-    duffy_logp_mesh = combine_spatial_inputs(duflon,duflat)
+    logp_mesh = combine_spatial_inputs(np.array(locs)[:,0],np.array(locs)[:,1])
     
     # Create the mean & its evaluation at the data locations.
     print data_mesh.shape
@@ -163,52 +163,41 @@ def make_model(lon,lat,covariate_values,n,datatype,
     # Probability of mutation in the promoter region, given that the other thing is a.
     p1 = pm.Uniform('p1', 0, .04, value=.01)
     
+    vivax_keys = set(covariate_values.keys())
+    vivax_keys.remove('africa')
+    vivax_covariate_values = dict([(k,covariate_values[k]) for k in vivax_keys])
+    covariate_value_dict = {'b': {'africa': covariate_values['africa']},
+                            '0': {},
+                            'v': vivax_covariate_values}
+    
     while not init_OK:
         try:
+            spatial_vars = zipmap(lambda k: covariance_submodel(k, logp_mesh, covariate_value_dict[k]), ['b,0,v'])
+            sp_sub = zipmap(lambda k: spatial_vars[k]['sp_sub'], ['b,0,v'])
+            V = zipmap(lambda k: spatial_vars[k]['V'], ['b,0,v'])
             
-            spatial_b_vars = covariance_submodel('b',duffy_logp_mesh,{'africa': covariate_values['africa']})
-            spatial_0_vars = covariance_submodel('0',duffy_logp_mesh,{})
-            spatial_v_vars = covariance_submodel('v',vivax_logp_mesh,vivax_covariate_values)
-            sp_sub_b = spatial_b_vars['sp_sub']
-            sp_sub_0 = spatial_0_vars['sp_sub']        
-            sp_sub_v = spatial_v_vars['sp_sub']
-                                
             # Loop over data clusters, adding nugget and applying link function.
-            eps_p_f0_d = []
-            p0_d = []
-            eps_p_fb_d = []
-            pb_d = []
+            f = zipmap(lambda k: spatial_vars[k]['sp_sub'].f_eval, ['b,0,v'])
+            eps_p_f_d = {'b':[], '0':[], 'v':[]}
+            p_d = {'b':[], '0': [], 'v': []}
+            eps_p_f = {}
+                    
+            for k in ['b','0','v']:    
+                for i in xrange(np.ceil(len(n)/float(grainsize))):
+                    sl = slice(i*grainsize,(i+1)*grainsize,None)                
+                    if sl.stop>sl.start:
+                    
+                        this_f[k] = f[k][fi[sl]]
 
-            V_b = spatial_b_vars['V']
-            V_0 = spatial_0_vars['V']     
-            V_v = spatial_v_vars['v']       
+                        # Nuggeted field in this cluster
+                        eps_p_f_d[k].append(pm.Normal('eps_p_f%s_%i'%(k,i), this_f[k], 1./V[k], value=np.random.normal(size=np.shape(this_f[k].value)), trace=False))
+                
+                        # The allele frequency
+                        p_d[k].append(pm.Lambda('p%s_%i'%(k,i),lambda lt=eps_p_f_d[k][-1]: invlogit(np.atleast_1d(lt)),trace=False))
+
+                # The fields plus the nugget
+                eps_p_f[k] = pm.Lambda('eps_p_f%s'%k, lambda eps_p_f_d=eps_p_f_d[k]: np.hstack(eps_p_f_d))
             
-            for i in xrange(np.ceil(len(n)/float(grainsize))):
-                sl = slice(i*grainsize,(i+1)*grainsize,None)
-                
-                if sl.stop>sl.start:
-                    this_fb = pm.Lambda('fb_%i'%i, lambda f=fb, sl=sl, fi=fi: f[fi[sl]], trace=False)
-                    this_f0 = pm.Lambda('f0_%i'%i, lambda f=f0, sl=sl, fi=fi: f[fi[sl]], trace=False)
-
-                    # Nuggeted field in this cluster
-                    eps_p_fb_d.append(pm.Normal('eps_p_fb_%i'%i, this_fb, tau_b, value=np.random.normal(size=np.shape(this_fb.value)), trace=False))
-                    eps_p_f0_d.append(pm.Normal('eps_p_f0_%i'%i, this_f0, tau_0, value=np.random.normal(size=np.shape(this_fb.value)), trace=False))
-                
-                    # The allele frequency
-                    pb_d.append(pm.Lambda('pb_%i'%i,lambda lt=eps_p_fb_d[-1]: invlogit(np.atleast_1d(lt)),trace=False))
-                    p0_d.append(pm.Lambda('p0_%i'%i,lambda lt=eps_p_f0_d[-1]: invlogit(np.atleast_1d(lt)),trace=False))
-        
-            # The fields plus the nugget
-            @pm.deterministic
-            def eps_p_fb(eps_p_fb_d = eps_p_fb_d):
-                """Concatenated version of eps_p_fb, for postprocessing & Gibbs sampling purposes"""
-                return np.hstack(eps_p_fb_d)
-
-            @pm.deterministic
-            def eps_p_f0(eps_p_f0_d = eps_p_f0_d):
-                """Concatenated version of eps_p_f0, for postprocessing & Gibbs sampling purposes"""
-                return np.hstack(eps_p_f0_d)
-        
             init_OK = True
         except pm.ZeroProbability, msg:
             print 'Trying again: %s'%msg
@@ -222,12 +211,11 @@ def make_model(lon,lat,covariate_values,n,datatype,
         sl_ind = int(i/grainsize)
         sub_ind = i%grainsize
         
-        if sl_ind == len(p0_d):
+        if sl_ind == len(p_d['b']):
             break
         
         # See duffy/doc/model.tex for explanations of the likelihoods.
-        p0 = pm.Lambda('p0_%i_%i'%(sl_ind,sub_ind), lambda p=p0_d[sl_ind], j=sub_ind: p[j], trace=False)
-        pb = pm.Lambda('pb_%i_%i'%(sl_ind,sub_ind), lambda p=pb_d[sl_ind], j=sub_ind: p[j], trace=False)
+        pb,p0,pv = map(lambda k: p_d[k][sl_ind][j], ['b,0,v'])
         
         if datatype[i]=='prom':
             cur_obs = [prom0[i], promab[i]]
@@ -268,6 +256,13 @@ def make_model(lon,lat,covariate_values,n,datatype,
                 np.array([g_freqs[key](pb,p0,p1) for key in ['aa','ab','a0','a1','bb','b0','b1','00','01','11']]), trace=False)
             np.testing.assert_almost_equal(p.value.sum(), 1)
             data_d.append(pm.Multinomial('data_%i'%i, p=p, n=n, value=cur_obs, observed=True))
+        
+        elif datatype[i]=='vivax':
+            cur_obs = np.array([vivax_pos[i], vivax_neg[i]])
+            n = np.sum(cur_obs)
+            pphe0 = pm.Lambda('pphe0_%i'%i, lambda pb=pb, p0=p0, p1=p1: (g_freqs['00'](pb,p0,p1)+g_freqs['01'](pb,p0,p1)+g_freqs['11'](pb,p0,p1)))
+            p = pm.Lambda('p_%i'%i, lambda pphe0=pphe0, pv=pv: pv*(1-pphe0), trace=False)
+            data_d.append(pm.Binomial('data_%i'%i, p=p, n=n, value=vivax_pos[i], observed=True))
             
         if np.any(np.isnan(cur_obs)):
             raise ValueError
