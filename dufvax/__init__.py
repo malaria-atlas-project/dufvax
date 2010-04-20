81eb03b7e0c66d9b151ed0ee94b69e8a60d73e85
Anand Patil <anand.prabhakar.patil@gmail.com>
2010-04-20 13:29:48 +0100

    Passing tis to step method correctly

--- dufvax/__init__.py
+++ dufvax/__init__.py
@@ -145,7 +145,7 @@ metadata_keys = ['disttol','ttol']
 def mcmc_init(M):
     for k in ['b','0']:
         M.use_step_method(GPEvaluationGibbs, M.sp_sub[k], M.V[k], M.eps_p_f_d[k], ti=duffy_ti)
-    M.use_step_method(GPEvaluationGibbs, M.sp_sub['v'], M.V['v'], M.eps_p_f_d['v'], ti=duffy_ti)
+    M.use_step_method(GPEvaluationGibbs, M.sp_sub['v'], M.V['v'], M.eps_p_f_d['v'], ti=vivax_ti)
     
     for k in ['b','0','v']:
         for epf in M.eps_p_f_d[k]:
