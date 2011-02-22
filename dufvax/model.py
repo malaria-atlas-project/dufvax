d8ac4557a752d36dfbc5cd729f81820aae399703
Anand Patil <anand.prabhakar.patil@gmail.com>
2011-02-22 11:28:19 +0000

    ...

--- dufvax/model.py
+++ dufvax/model.py
@@ -130,7 +130,7 @@ def covariance_submodel(suffix, ra, mesh, covariate_keys, ui, fname, temporal=Fa
         return pm.gp.Mean(pm.gp.zero_fn)
     
     # Create the GP submodel    
-    sp_sub = pm.gp.GPSubmodel('sp_sub_%s'%suffix,M,C,mesh)
+    sp_sub = pm.gp.GPSubmodel('sp_sub_%s'%suffix,M,C,mesh,tally_f=False)
     sp_sub.f.trace=False
     sp_sub.f_eval.value = sp_sub.f_eval.value - sp_sub.f_eval.value.mean()    
     
